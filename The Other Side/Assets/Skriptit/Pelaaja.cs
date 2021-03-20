9cc1482d641b6caaff38ac1c2ad9fbc47417be15
qqepijack <66687334+joniloo@users.noreply.github.com>
2021-03-20 17:59:12 +0200

    pelaaja

---
+++ The Other Side/Assets/Skriptit/Pelaaja.cs
@@ -0,0 +1,58 @@
+
+using UnityEngine;
+[RequireComponent(typeof(CharacterController))]
+public class Pelaaja : MonoBehaviour
+{
+
+    public float walkingSpeed = 7.5f;
+    public float runningSpeed = 11.5f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
+    public Camera playerCamera;
+
+    CharacterController characterController;
+    [HideInInspector]
+    public Vector3 moveDirection = Vector3.zero;
+    [HideInInspector]
+    public bool canMove = true;
+
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+
+    }
+
+    void Update()
+    {
+
+        Vector3 right = transform.TransformDirection(Vector3.right);
+
+        bool isRunning = Input.GetKey(KeyCode.LeftShift);
+
+        float curSpeedY = canMove ? (isRunning ? runningSpeed : walkingSpeed) * Input.GetAxis("Horizontal") : 0;
+        float movementDirectionY = moveDirection.y;
+        moveDirection = (right * curSpeedY);
+
+        if (Input.GetButton("Jump") && canMove && characterController.isGrounded)
+        {
+            moveDirection.y = jumpSpeed;
+        }
+        else
+        {
+            moveDirection.y = movementDirectionY;
+        }
+
+        if (!characterController.isGrounded)
+        {
+            moveDirection.y -= gravity * Time.deltaTime;
+        }
+
+
+        characterController.Move(moveDirection * Time.deltaTime);
+
+    }
+}
